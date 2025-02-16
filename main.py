e210f624bc94756352589a87fc7135f4e0b3fed1
angrypigs <angrypigs.tnt@gmail.com>
2025-02-16 18:35:35 +0100

    Changed the behavior of peter alert lol

--- main.py
+++ main.py
@@ -1,38 +1,45 @@
 from time import sleep
+from random import randint
 from peter import Peter
-from math import pi, cos, sin
-from pyautogui import size as screen_size
-import asyncio
-
+from pyautogui import size
+import tkinter as tk
+from threading import Thread
 
 class App:
 
     def __init__(self) -> None:
-        self.QUANTITY = 30
-        self.peters: list[Peter | None] = [None for x in range(self.QUANTITY)]
-        w, h = screen_size()
-        r = h//3
-        w, h = w//2, h//2
-        self.POINTS = tuple((int(w + r * cos(2 * pi * i / self.QUANTITY)), 
-                        int(h + r * sin(2 * pi * i / self.QUANTITY))) 
-                        for i in range(self.QUANTITY))
-        self.current_peter = 0
-        asyncio.run(self.peter_loop())
-
-    async def create_peter(self, index: int) -> None:
-        await asyncio.sleep(0.01)
-        self.peters[index] = Peter(self.POINTS[index][0], self.POINTS[index][1])
-
-    async def peter_loop(self) -> None:
-        for i in range(200):
-            await self.create_peter(self.current_peter)
-            if self.peters[self.current_peter - 5] is not None:
-                self.peters[(self.current_peter - 5) % self.QUANTITY].quit_peter()
-                self.peters[(self.current_peter - 5) % self.QUANTITY] = None
-            self.current_peter = (self.current_peter + 1) % self.QUANTITY
-            await asyncio.sleep(0.01)
-
-
+        self.PETER_WIDTH = 300
+        self.PETER_HEIGHT = 200
+        self.PETER_GEOMETRY = f"{self.PETER_WIDTH}x{self.PETER_HEIGHT}"
+        self.screen_size = size()
+        self.root = tk.Tk()
+        self.peters: list[Peter] = [Peter(self.root, (self.screen_size[0] - self.PETER_WIDTH) // 2,
+                                          (self.screen_size[1] - self.PETER_HEIGHT) // 2, 
+                                          self.PETER_WIDTH, self.PETER_HEIGHT)]
+        self.root.withdraw()
+        Thread(target=self.peter_loop, daemon=True).start()
+        self.root.mainloop()
+    
+    def peter_loop(self) -> None:
+        while True:
+            peters_to_remove: list[Peter] = []
+            peter_flag = False
+            for peter in self.peters:
+                if not peter.peter:
+                    peters_to_remove.append(peter)
+                elif peter.new_peter:
+                    peter_flag = True
+            for peter in peters_to_remove:   
+                self.peters.remove(peter)
+            if peter_flag:
+                self.peters.append(Peter(self.root, randint(200, self.screen_size[0] - self.PETER_WIDTH - 200),
+                                          randint(200, self.screen_size[1] - self.PETER_HEIGHT - 200), 
+                                          self.PETER_WIDTH, self.PETER_HEIGHT))
+            if len(self.peters) > 100:
+                self.peters = []
+            if not self.peters:
+                self.root.destroy()
+            sleep(0.05)
 
 if __name__ == "__main__":
     App()
--- peter.py
+++ peter.py
@@ -1,31 +1,29 @@
 from os import path
 import sys
 import tkinter as tk
-from threading import Thread
 
 class Peter:
 
-    def __init__(self, w: float, h: float) -> None:
-        Thread(target=self.__init_peter, args=(w, h, ), daemon=True).start()
-
-    def __init_peter(self, w: int, h: int) -> None:
-        self.WIDTH = 300
-        self.HEIGHT = 200
-        self.master = tk.Tk()
+    def __init__(self, root: tk.Tk, w: float, h: float, width: int, height: int) -> None:
+        self.WIDTH = width
+        self.HEIGHT = height
+        self.peter = True
+        self.new_peter = False
+        self.master = tk.Toplevel(root)
+        self.master.geometry(f"{width}x{height}+{w - self.WIDTH // 2}+{h - self.HEIGHT // 2}")
         self.master.title("Peter alert")
-        self.master.geometry(f"{self.WIDTH}x{self.HEIGHT}+{w - self.WIDTH // 2}+{h - self.HEIGHT // 2}")
         self.master.resizable(False, False)
         self.master.iconbitmap(default = self.res_path("peter griffin icon.ico"))
         self.pixel = tk.PhotoImage(master=self.master, height = 1, width = 1)
         self.canvas = tk.Canvas(self.master, height=self.HEIGHT, width=self.WIDTH,
                                 bd=0, highlightthickness=0)
         self.canvas.place(x = 0, y = 0)
-        peter_image = tk.PhotoImage(master=self.master, file = self.res_path("peter griffin.png"))
+        self.peter_image = tk.PhotoImage(file = self.res_path("peter griffin.png"))
         self.canvas.create_image(self.WIDTH // 2, self.HEIGHT / 4, anchor = 'center',
-                                 image = peter_image)
-        tk.Button(self.canvas, image = self.pixel, compound = 'c', height = 30, width = 100, text = "OK"
+                                 image = self.peter_image)
+        tk.Button(self.canvas, image = self.pixel, compound = 'c', height = 30, width = 100, text = "OK", command=self.quit_peter
                   ).place(x = self.WIDTH // 2, y = self.HEIGHT / 4 * 3, anchor = 'center')
-        self.master.mainloop()
+        self.master.protocol("WM_DELETE_WINDOW", self.peter_signal)
 
     def res_path(self, rel_path: str) -> str:
         try:
@@ -39,5 +37,9 @@ class Peter:
         self.master.after(20, self.destroy_peter)
 
     def destroy_peter(self) -> None:
+        self.peter = False
         self.master.destroy()
+    
+    def peter_signal(self) -> None:
+        self.new_peter = True
 
